8dedf59f77ca37f308a0c4780cbe87396f2c9b63
Sterus4 <368581@niuitmo.ru>
2025-08-01 21:24:24 +0300

    init

---
+++ scripts/build-project.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set BUILD_DIR=build\app
+set OUTPUT=%BUILD_DIR%\myapp.exe
+set MAIN_FILE=app\main.go
+
+if not exist "%BUILD_DIR%" (
+    echo Creating folder: %BUILD_DIR%
+    mkdir "%BUILD_DIR%"
+)
+
+echo Compiling project
+go build -o "%OUTPUT%" "%MAIN_FILE%"
